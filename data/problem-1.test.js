4483d4bfec8e34d6fbcd58e74d74ec1ea19146b8
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-01-20 23:53:24 -0600

    sending test results back to user

--- data/problem-1.test.js
+++ data/problem-1.test.js
@@ -2,9 +2,9 @@ const sum = (num1, num2) => {
   // Add Code Below
   return num1 + num2;
 
+  
   //Add Code Above
-};
-
+}
 const testCases = [
   { num1: 1, num2: 2, result: 3 },
   { num1: 5, num2: 10, result: 15 },
--- pages/api/testcode/[testname].ts
+++ pages/api/testcode/[testname].ts
@@ -2,13 +2,25 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 import { execSync } from 'child_process';
 
-const testScript = `describe('Sum 2 Numbers', () => {
-  test('User create a function called sum', () => {
-    expect(typeof sum).toBe("function");
-  })
+const testScript = `const testCases = [
+  { num1: 1, num2: 2, result: 3 },
+  { num1: 5, num2: 10, result: 15 },
+  { num1: -1, num2: -4, result: -5 },
+];
 
-  
-})`;
+describe('Sum 2 Numbers', () => {
+  test('User created a function called sum', () => {
+    expect(typeof sum).toBe('function');
+  });
+
+  test('Function returns a number', () => {
+    expect(typeof sum(1, 2)).toBe('number');
+  });
+
+  test.each(testCases)('sum($num1, $num2) returns $result.', ({ num1, num2, result }) => {
+    expect(sum(num1, num2)).toBe(result);
+  });
+});`;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const testname = req.query.testname;
@@ -19,16 +31,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { userCode } = req.body;
 
     fs.writeFileSync(`data/${testname}.test.js`, `${userCode}\n`);
-    fs.appendFileSync(
-      `data/${testname}.test.js`,
-      `test('sum 2 numbers', () => {
-      expect(sum(1, 2)).toBe(3);
-    });`,
-    );
-    execSync(`jest ${testname}`);
+    fs.appendFileSync(`data/${testname}.test.js`, testScript);
+    execSync(`jest ${testname} --json --outputFile=data/${testname}-results.json`);
+    let results = JSON.parse(fs.readFileSync(`data/${testname}-results.json`, 'utf-8'));
+    let testResults = results.testResults[0].assertionResults;
 
-    console.log(userCode);
-    res.status(200).json({ message: 'test code submitted' });
+    res.status(200).json({ testResults });
   }
 };
 
