98a29c7e843f0de0aedc0006aba1bd94363d8279
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-01-26 14:44:48 -0600

    added a new exercise

--- data/testLogic/index.ts
+++ data/testLogic/index.ts
@@ -78,9 +78,30 @@ const countTheVowelsTestScript = `const testCases = [
   })
 });`;
 
+const sortTheDaysTestScript = `const testCases = [
+  { str: 'mon,wed,tues,sat', result: 'mon,tues,wed,sat' },
+  { str: 'sun,sat,fri,thurs,wed,tues,mon', result: 'mon,tues,wed,thrus,fri,sat,sun' },
+  { str: 'wed,tues,sat,fri', result: 'tues,wed,fri,sat' }
+ ];
+ 
+ describe('Sort The Days', () => {
+  test('User created a function called sortDays', () => {
+    expect(typeof sortDays).toBe('function');
+  });
+
+  test('sortDays returns a string', () => {
+    expect(typeof sortDays('mon,wed,tues')).toBe('string')
+  });
+
+  test.each(testCases)('sortDays($str) returns $result.', ({ str, result }) => {
+    expect(sortDays(str)).toBe(result);
+  })
+});`;
+
 export const testScripts = {
   sumTwoIntsTestScript,
   sortArrayIntsTestScript,
   largestNumInArrayTestScript,
   countTheVowelsTestScript,
+  sortTheDaysTestScript,
 };
